9b6702bfcaa9cfe0254cae6e83f52f9894726298
n0iz <n0iz@n0iz.ddns.net>
2017-05-30 19:15:48 +0200

    implem set

--- src/components/RegistrySetter.js
+++ src/components/RegistrySetter.js
@@ -3,20 +3,25 @@ import React, { Component } from 'react'
 export default class ProductionCreator extends Component {
 	constructor(props) {
 		super(props)
-		this.state = {}
+		this.state = {address: ""}
 
 		this.createRegistry = this.createRegistry.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+		this.setRegistry = this.setRegistry.bind(this)
 	}
 	render() { return (
-		<form onSubmit={this.createRegistry}>
+		<form onSubmit={this.setRegistry}>
 			<label>
 				Registry Address:
-				<input type="text" name="desc" value={this.state.desc} onChange={this.handleChange} />
+				<input type="text" name="address" value={this.state.address} onChange={this.handleChange} />
 			</label>
 			<input type="submit" value="Set Registry" />
-			<input type="submit" value="Create Registry" />
+			<button onClick={this.createRegistry}>Create Registry</button>
 		</form>
     )}
+	handleChange(event) {
+		this.setState({address: event.target.address})
+	}
 	createRegistry(event) {
 		event.preventDefault();
 		const loadContract = require("core").loadContract
@@ -30,6 +35,24 @@ export default class ProductionCreator extends Component {
             alert("created registry" + instance.address)
 			console.log(instance)
 			this.props.setRegistry(instance)
+			this.setState({address: instance.address})
+        })
+	}
+	setRegistry(event) {
+		event.preventDefault();
+		const loadContract = require("core").loadContract
+		var ProductionRegistry = loadContract("ProductionRegistry")
+		ProductionRegistry.defaults({
+			from: "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1",
+			gas: 4712388,
+			gasPrice: 100000000000,
+			address: this.state.address
+		})
+		ProductionRegistry.deployed().then((instance) => {
+            alert("set registry" + instance.address)
+			console.log(instance)
+			this.props.setRegistry(instance)
+			this.setState({address: instance.address})
         })
 	}
 }
